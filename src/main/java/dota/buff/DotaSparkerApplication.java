9d441fb465f988597f96f2bb3a8f16b8ff778639
Roman Ihnatov <romamaster42@gmail.com>
2021-07-09 21:44:08 +0300

    Initial commit

---
+++ src/main/java/dota/buff/DotaSparkerApplication.java
@@ -0,0 +1,13 @@
+package dota.buff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaSparkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotaSparkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dota/buff/DotaSparkerApplicationTests.java
@@ -0,0 +1,13 @@
+package dota.buff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaSparkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
