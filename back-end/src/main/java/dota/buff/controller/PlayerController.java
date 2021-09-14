22223273a0b85d7478bd765eb444cb918695efac
spikyzero <nikeslavko0906@gmail.com>
2021-09-14 18:34:08 +0300

    The latest commit ever, because i fixed all mistakes, include '*' in annotation

--- back-end/src/main/java/dota/buff/controller/PlayerController.java
+++ back-end/src/main/java/dota/buff/controller/PlayerController.java
@@ -8,7 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
